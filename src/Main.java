1e9addfb2f14b492ac9a74e74c80415d28cdd603
Anatoli Weikum <weikum.anatoli@gmail.com>
2023-12-26 22:08:57 +0100

    1. create todos in main class 2. added flow chart and class overview in draw.io file

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    // TODO 1: first choice - report/Drink/off
+
+
+    // TODO 2: order a drink
+
+
+    // TODO 3: pay the bill
+
+
+    // TODO 4: make drink
 }
