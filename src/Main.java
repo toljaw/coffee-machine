0113cf2b4830c6c50dd1e58c29c65dc0b28b4997
Anatoli Weikum <weikum.anatoli@gmail.com>
2023-12-27 09:48:03 +0100

    create resources and the order/report/off function

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,61 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int resWater = 2000;
+        int resMilk = 2000;
+        int resCoffee = 2000;
+        double resMoney = 0;
+
+        boolean machineIsOn = true;
+        while (machineIsOn){
+            System.out.println("Please choose your drink/report/off...");
+
+
+            // TODO 1: first choice - report/Drink/off
+            Scanner scan = new Scanner(System.in);
+            String choice = scan.nextLine();
+
+            switch (choice){
+                case "off" -> machineIsOn = false;
+                case "report" -> System.out.println("REPORT: \nWater: " + resWater + "\nMilk: " + resMilk + "\nCoffee: " + resCoffee + "\nMoney: " + resMoney);
+                // TODO 2: order a drink
+                case "espresso" -> {
+                    if (resWater >= 50 && resMilk >= 0 && resCoffee >= 18){
+                        System.out.println("espresso");
+                    }else{
+                        System.out.println("not enough ressource... please another order...");
+                    }
+                }
+                case "latte" -> {
+                    if (resWater >= 200 && resMilk >= 150 && resCoffee >= 24){
+                        System.out.println("latte");
+                    }else{
+                        System.out.println("not enough ressource... please another order...");
+                    }
+                }
+                case "cappuccino" -> {
+                    if (resWater >= 250 && resMilk >= 100 && resCoffee >= 24){
+                        System.out.println("cappuccino");
+                    }else{
+                        System.out.println("not enough ressource... please another order...");
+                    }
+                }
+                default -> System.out.println("I'm sorry but there is no such order... please make a new one...");
+            }
+
+
+
+
+            // TODO 3: pay the bill
+
+
+            // TODO 4: make drink
+
+        }
+
+
+
     }
-    // TODO 1: first choice - report/Drink/off
 
-
-    // TODO 2: order a drink
-
-
-    // TODO 3: pay the bill
-
-
-    // TODO 4: make drink
 }
